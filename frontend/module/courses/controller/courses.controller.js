9e4d47053e7c0909ada2cef5def86d9f42420843
gionaico <gmc.yanez@gmail.com>
2018-05-12 21:47:08 +0200

    En proceso de paginacion

--- frontend/module/courses/controller/courses.controller.js
+++ frontend/module/courses/controller/courses.controller.js
@@ -1,5 +1,31 @@
 console.log("courseController");
 
 appLibra.controller('coursesCategoryCtrl', function ($scope, services, coursesCategory) {
+    window.scrollTo(0, 0);
+    
+    var category=coursesCategory.category;
     console.log(coursesCategory);
+    console.log("categoria es:  "+coursesCategory.category);
+    var datos=coursesCategory.datos;
+    $scope.courseslist=[];
+    
+    
+    console.log(category);
+
+    if (category!=" ") {
+        console.log("lleno");
+        for (var i =0; i<datos.length; i++) {
+            if (datos[i].subject==category) 
+                $scope.courseslist.push(datos[i]);
+        }
+    } else {
+        console.log("vacio");
+        $scope.courseslist=coursesCategory.datos;
+    }
+
+    /*var rounded = Math.round(12.1+0.4);
+    console.log(rounded);*/
+
+    console.log($scope.courseslist);
+    var mensaje="No hay courses con estos parametros"
 });
--- frontend/module/homepage/controller/homepage.controller.js
+++ frontend/module/homepage/controller/homepage.controller.js
@@ -1,8 +1,8 @@
-console.log("entra en home js");
-
-appLibra.controller('homepageCtrl', function ($scope, services, cursosMasValorados, categoriaCourses) {
-    console.log(cursosMasValorados);
-    console.log(categoriaCourses);
+/*console.log("entra en home js");
+*/
+appLibra.controller('homepageCtrl', function ($scope, services, cursosMasValorados, categoriaCourses) {/*
+    console.log(cursosMasValorados);*//*
+    console.log(categoriaCourses);*/
     var limite = 0;
     var limite_b = 2;
     var limiteCat = 0;
@@ -17,19 +17,19 @@ appLibra.controller('homepageCtrl', function ($scope, services, cursosMasValorad
         $scope.categories.push(categoriaCourses[i]);
     }
 
-    console.log($scope.courses);
+/*    console.log($scope.courses);*/
     $scope.moreCourses=function(){
         limite=limite+3;
         limite_b=limite_b+3;
-        console.log(limite+" "+limite_b);
+        /*console.log(limite+" "+limite_b);*/
         if (limite_b>cursosMasValorados.length) {
             for (var i =limite; i<cursosMasValorados.length; i++) {
-                console.log(cursosMasValorados[i]);
+                /*console.log(cursosMasValorados[i]);*/
                 $scope.courses.push(cursosMasValorados[i]);
             }
         } else {
             for (var i =limite; i<=limite_b; i++) {
-                console.log(cursosMasValorados[i]);
+                /*console.log(cursosMasValorados[i]);*/
                 $scope.courses.push(cursosMasValorados[i]);
             }
         }
@@ -38,15 +38,15 @@ appLibra.controller('homepageCtrl', function ($scope, services, cursosMasValorad
     $scope.moreCategories=function(){
         limiteCat=limiteCat+3;
         limiteCat_b=limiteCat_b+3;
-        console.log(limiteCat+" "+limiteCat_b);
+        /*console.log(limiteCat+" "+limiteCat_b);*/
         if (limiteCat_b>categoriaCourses.length) {
             for (var i =limiteCat; i<categoriaCourses.length; i++) {
-                console.log(categoriaCourses[i]);
+                /*console.log(categoriaCourses[i]);*/
                 $scope.categories.push(categoriaCourses[i]);
             }
         } else {
             for (var i =limiteCat; i<=limiteCat_b; i++) {
-                console.log(categoriaCourses[i]);
+                /*console.log(categoriaCourses[i]);*/
                 $scope.categories.push(categoriaCourses[i]);
             }
         }
